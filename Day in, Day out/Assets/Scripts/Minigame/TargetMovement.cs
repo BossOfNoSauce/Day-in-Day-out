18392ca259e65e5446e3f1b4c30430969813c32a
BossOfTheSauce5728 <ethcichowicz5728@gmail.com>
2023-03-31 14:55:18 -0700

    idek man

--- Day in, Day out/Assets/Scripts/Minigame/TargetMovement.cs
+++ Day in, Day out/Assets/Scripts/Minigame/TargetMovement.cs
@@ -5,17 +5,16 @@ using UnityEngine;
 public class TargetMovement : MonoBehaviour
 {
     public float speed = 0.15f;
-    public float speed2 = 0.2f;
-    Vector3 pointA;
-    Vector3 pointB;
+   
+   
     float horizontalInput;
     
     public float swayTime = 5f;
-    float time = 0f;
+    public float time = 0f;
     public bool GameIsActive = true;
     public Vector3 right;
     public Rigidbody m_rigidbody;
-    public float m_Thrust = 5f;
+   
 
     void Start()
     {
@@ -30,12 +29,16 @@ public class TargetMovement : MonoBehaviour
 
     void Update()
     {
-        time += Time.deltaTime;
-        if (time > swayTime)
+        if(GameIsActive == true)
         {
-            m_rigidbody.velocity = -1 * Mathf.Sign(m_rigidbody.velocity.x) * right;
-            time = 0f;
+            time += Time.deltaTime;
+            if (time > swayTime)
+            {
+                m_rigidbody.velocity = -1 * Mathf.Sign(m_rigidbody.velocity.x) * right;
+                time = 0f;
+            }
         }
+       
      
       //set barriers that when on collided, it game overs.
 
@@ -43,17 +46,7 @@ public class TargetMovement : MonoBehaviour
 
     }
 
-    void MoveObj()
-    {
-       
-        
-
-
-
-
-        
-       
-    }
+    
 
    
 
