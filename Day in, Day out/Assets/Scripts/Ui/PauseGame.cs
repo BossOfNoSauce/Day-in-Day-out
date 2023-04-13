609e66a54231b43586d383491f65f706a1bbb11c
BossOfTheSauce5728 <ethcichowicz5728@gmail.com>
2023-04-13 14:48:07 -0700

    ASDFGH

--- Day in, Day out/Assets/Scripts/Ui/PauseGame.cs
+++ Day in, Day out/Assets/Scripts/Ui/PauseGame.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseGame : MonoBehaviour
 {
@@ -39,6 +40,11 @@ public class PauseGame : MonoBehaviour
             
         
        
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
     }
 
     void Start()
