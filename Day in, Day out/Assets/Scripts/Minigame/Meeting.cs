95b20ceb4a2a3ad9bd1041e30fc4d30c714e2103
EthanChickhoewicks <128403760+DrClober63@users.noreply.github.com>
2023-04-17 10:12:20 -0700

    meeting time

---
+++ Day in, Day out/Assets/Scripts/Minigame/Meeting.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Meeting : MonoBehaviour, Iinteractable 
+{
+    [SerializeField] private string prompt;
+
+    public string InteractionPrompt => prompt;
+
+    public GameObject manager;
+    GameManager gameManager;
+
+    public Vector3 playerPosition;
+
+    public GameObject Camera;
+    PlayerMovement playerMovement;
+    public GameObject Player;
+    public GameObject MainCam;
+
+    public bool Interact(Interactor interactor)
+    {
+        //this is what happenes when you interact
+        Debug.Log("Meeting Time");
+        StartCoroutine(MeetingTime());
+        return true;
+    }
+
+    void Awake()
+    {
+        playerMovement = Player.GetComponent<PlayerMovement>();
+        gameManager = manager.GetComponent<GameManager>();
+    }
+   
+    public IEnumerator MeetingTime()
+    {
+        {
+            playerMovement.InGame = true;
+            gameManager.gameActive = true;
+            yield return new WaitForSeconds(3);
+            Player.transform.position = new Vector3(-18, 5.5f, -54);
+        }
+
+
+    }
+}
