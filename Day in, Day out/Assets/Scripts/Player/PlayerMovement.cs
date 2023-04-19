abaab7c1446ea614cb2a0f70600e84dd8e0b8499
BossOfTheSauce5728 <ethcichowicz5728@gmail.com>
2023-04-19 15:37:30 -0700

    we

--- Day in, Day out/Assets/Scripts/Player/PlayerMovement.cs
+++ Day in, Day out/Assets/Scripts/Player/PlayerMovement.cs
@@ -36,6 +36,21 @@ public class PlayerMovement : MonoBehaviour
     public bool InGame = false; //sets the minigames to freeze movement. doesnt fuckin work
 
 
+    public float stamina = 100f;
+
+    
+    public float runSpeed = 10f;
+    public float walkSpeed = 5f;
+
+    public float drainRate = 1f;
+    public float reChargeRate = 1f;
+
+    public float fatigueTimer = 0f;
+    bool isFatigued;
+
+    bool isRunning;
+
+
     private void Awake()
     {
         playerCam = Camera.GetComponent<FirstPersonCameraRotation>();
@@ -82,6 +97,70 @@ public class PlayerMovement : MonoBehaviour
         {
             footstepsSound.enabled = false;
         }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            if (stamina > 0 && !isFatigued)
+            {
+                moveSpeed = runSpeed;
+                isRunning = true;
+            }
+            else
+
+            if (isRunning || isFatigued)
+            {
+                moveSpeed = walkSpeed;
+                isRunning = false;
+
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            if (isRunning || isFatigued)
+            {
+                moveSpeed = walkSpeed;
+                isRunning = false;
+            }
+        }
+
+        if (isRunning)
+        {
+            stamina -= Time.deltaTime * drainRate;
+        }
+        else
+
+        if (!isRunning && stamina > 0 && stamina < 100)
+        {
+            stamina += Time.deltaTime * reChargeRate;
+        }
+
+
+
+        if (stamina <= 0f && fatigueTimer <= 3)
+        {
+            fatigueTimer += Time.deltaTime;
+            isFatigued = true;
+        }
+        else
+
+        if (fatigueTimer >= 3)
+        {
+            stamina += Time.deltaTime * reChargeRate;
+            isFatigued = false;
+            fatigueTimer = 0;
+        }
+
+        if (stamina < 0f)
+        {
+            stamina = 0f;
+        }
+
+        if (stamina > 100f)
+        {
+            stamina = 100f;
+        }
+
 
 
     }
