fcbebbe0c518a0edcefd2821297e7bd60d8d98eb
EthanChickhoewicks <128403760+DrClober63@users.noreply.github.com>
2023-04-20 10:26:32 -0700

    qWAsQS

--- Day in, Day out/Assets/Scripts/Minigame/Meeting/Meeting.cs
+++ Day in, Day out/Assets/Scripts/Minigame/Meeting/Meeting.cs
@@ -19,12 +19,32 @@ public class Meeting : MonoBehaviour, Iinteractable
     public GameObject MainCam;
     Collider collider;
 
+    public GameObject Top;
+    public GameObject Bottom;
+
+    public float speed;
+    public Transform topTarget;
+    public Transform bottomTarget;
+
+    public Rigidbody TRB;
+    public Rigidbody BRB;
+    
     public bool Interact(Interactor interactor)
     {
         //this is what happenes when you interact
         Debug.Log("Meeting Time");
         StartCoroutine(MeetingTime());
         return true;
+    }
+
+    void Sleepy()
+    {
+        float step = speed * Time.deltaTime;
+        TRB.MovePosition(topTarget.position);
+        BRB.MovePosition(bottomTarget.position);
+
+       //Top.transform.position = Vector3.MoveTowards(Top.transform.position, topTarget.position, step);
+       // Bottom.transform.position = Vector3.MoveTowards(Bottom.transform.position, bottomTarget.position, step);
     }
 
     void Awake()
@@ -42,6 +62,8 @@ public class Meeting : MonoBehaviour, Iinteractable
             collider.enabled = false;
             yield return new WaitForSeconds(3);
             Player.transform.position = new Vector3(-19, 6.5f, -65);
+            yield return new WaitForSeconds(3.25f);
+            Sleepy();
         }
 
 
