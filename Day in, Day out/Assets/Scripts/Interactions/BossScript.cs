08ed3b0684e4b02adf4a3436a13c64870526398d
EthanChickhoewicks <128403760+DrClober63@users.noreply.github.com>
2023-04-28 08:49:21 -0700

    dytrxs

--- Day in, Day out/Assets/Scripts/Interactions/BossScript.cs
+++ Day in, Day out/Assets/Scripts/Interactions/BossScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BossScript : MonoBehaviour, Iinteractable
 {
@@ -15,6 +16,7 @@ public class BossScript : MonoBehaviour, Iinteractable
     public  AudioSource audioSource;
     public AudioClip day1monologue;
     public AudioClip Buzzer;
+    public GameObject textBox;
     // Start is called before the first frame update
     void Start()
     {
@@ -42,7 +44,34 @@ public class BossScript : MonoBehaviour, Iinteractable
             BossImage.SetActive(true);
             yield return new WaitForSeconds(1);
             audioSource.PlayOneShot(day1monologue, 1);
-            yield return new WaitForSeconds(34);
+            yield return new WaitForSeconds(1);
+            textBox.GetComponent<Text>().text = "Well, well, well, look who we got here";
+            yield return new WaitForSeconds(3);
+            textBox.GetComponent<Text>().text = "Good to see you kid";
+            yield return new WaitForSeconds(1.5f);
+            textBox.GetComponent<Text>().text = "Outta everyone is this office, you was the one I was the least worried about";
+            yield return new WaitForSeconds(4);
+            textBox.GetComponent<Text>().text = "Don't worry about turnin' in your book";
+            yield return new WaitForSeconds(1.7f);
+            textBox.GetComponent<Text>().text = "I trust ya enough that you're not gonna toss it or somethin'";
+            yield return new WaitForSeconds(3);
+            textBox.GetComponent<Text>().text = "But, I must say, I apologize for pushing these new rules on ya kid";
+            yield return new WaitForSeconds(4);
+            textBox.GetComponent<Text>().text = "Our business, is frankly in disarray";
+            yield return new WaitForSeconds(3);
+            textBox.GetComponent<Text>().text = "Because this establishment is full of";
+            yield return new WaitForSeconds(2);
+            textBox.GetComponent<Text>().text = "LAZY";
+            yield return new WaitForSeconds(1);
+            textBox.GetComponent<Text>().text = "GOOD FOR NOTHING";
+            yield return new WaitForSeconds(1);
+            textBox.GetComponent<Text>().text = "MALINGERERS";
+            yield return new WaitForSeconds(1.5F);
+            textBox.GetComponent<Text>().text = "I gotta do what I gotta do, for the greater good of this company";
+            yield return new WaitForSeconds(3.5f);
+            textBox.GetComponent<Text>().text = "So, you keep up the good work kid, and don't dissappoint me";
+            yield return new WaitForSeconds(4);
+            textBox.GetComponent<Text>().text = " ";
             daySystem.BossCheck = true;
             Debug.Log(daySystem.BossCheck);
             BossImage.SetActive(false);
@@ -53,6 +82,5 @@ public class BossScript : MonoBehaviour, Iinteractable
        // }
        // yield return new WaitForSeconds(1);
     }
-
     
 }
