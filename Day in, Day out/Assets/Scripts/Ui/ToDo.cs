5d76acec455cec2a7c5a85df5a382dcaab0fb4d8
cooljam206 <4017157@g.highlineschools.org>
2023-05-02 08:20:30 -0700

    working on todo lsit

---
+++ Day in, Day out/Assets/Scripts/Ui/ToDo.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ToDo : MonoBehaviour
+{
+    //to-do
+    //[]set tab to show/hide the todo list ui element by sliding
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
