a697aeb99d9ca5f3cf5c3fbbf5869a5040e58288
BossOfTheSauce1 <4028548@stu.highlineschools.org>
2023-05-22 09:59:19 -0700

    q

--- Day in Day out/Assets/Scripts/Minigame/Computer/HAND.cs
+++ Day in Day out/Assets/Scripts/Minigame/Computer/HAND.cs
@@ -10,10 +10,13 @@ public class HAND : MonoBehaviour
 
     public float offset = 3f;
 
+    public Color[] colors;
+
+   
     // Start is called before the first frame update
     void Start()
     {
-        
+        GetComponent<Renderer>().material.color = colors[Random.Range(0, colors.Length)];
     }
 
     // Update is called once per frame
@@ -21,6 +24,8 @@ public class HAND : MonoBehaviour
     {
         if(HandActive == true)
         {
+            
+
             pos = Input.mousePosition;
 
             pos.z = offset;
