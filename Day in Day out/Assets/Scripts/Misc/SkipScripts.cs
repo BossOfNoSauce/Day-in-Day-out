018509e68fe9d7d3cf794ecd0b77d4d32177382d
BossOfTheSauce1 <4028548@stu.highlineschools.org>
2023-05-25 10:24:37 -0700

    fixing files

---
+++ Day in Day out/Assets/Scripts/Misc/SkipScripts.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SkipScripts : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("[1]"))
+        {
+
+        }
+        if (Input.GetKeyDown("[2]"))
+        {
+
+        }
+        if (Input.GetKeyDown("[3]"))
+        {
+
+        }
+        if (Input.GetKeyDown("[4]"))
+        {
+
+        }
+        if (Input.GetKeyDown("[5]"))
+        {
+
+        }
+
+
+    }
+}
